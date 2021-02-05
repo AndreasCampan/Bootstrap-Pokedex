14fefc579c16affcea09ca18f9dd09d147ccd7a9
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-02-05 14:10:49 -0600

    Created Files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert('Hello World')
+
+let favoriteFood = 'Yellow Watermelon';
+document.write(favoriteFood);
+
+// Note: this is how you write a comment on a line
+
+/* And this is how you write a comment on
+multiple lines. Why?... because you can*/
+
+let simpleaddition = 2 + 2;
+document.write(simpleaddition)

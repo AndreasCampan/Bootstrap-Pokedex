93e6255fd5f85591eee8f5d71a45dbdf65fdeae5
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-02-08 20:43:00 -0600

    Removed old content and added task's data
    Introduced a pokedex object with an array of key-value pairs for the start of my pokedex application.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-alert('Hello World!')
-
-let favoriteFood = 'Yellow Watermelon';
-document.write(favoriteFood);
-
-// Note: this is how you write a comment on a line
-
-/* And this is how you write a comment on
-multiple lines. Why?... because you can*/
-
-let simpleaddition = 2 + 2;
-document.write(simpleaddition)
+pokemonList = [
+  {name = 'Bulbasur', height = 70, weight = 15.2, type = ['grass','poison']},
+  {name = 'Charmander', height = 60, weight = 18.7, type = ['fire']},
+  {name = 'Squirtle', height = 50, weight = 19.8, type = ['water']},
+  {name = 'Alakazam', height = 150, weight = 105.8, type = ['psychic']},
+  {name = 'Gengar', height = 150, weight = 89.3, type = ['ghost','posion']},
+  {name = 'Hitmonchan', height = 140, weight = 110.7, type = ['fighting']},
+  {name = 'Gyarados', height = 650, weight = 518.1, type = ['water','flying']},
+  {name = 'Snorlax', height = 210, weight = 1014.1, type = ['normal']},
+  {name = 'Dragonite', height = 220, weight = 463, type = ['dragon', 'flying']}
+];

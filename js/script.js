44fb6bb5288f4ec67f58c7466e3d94dc11692f8d
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-02-08 21:14:16 -0600

    Changed syntax to correct format with colons
    Objects need a colon not equal sing for key value pairs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 pokemonList = [
-  {name = 'Bulbasur', height = 70, weight = 15.2, type = ['grass','poison']},
-  {name = 'Charmander', height = 60, weight = 18.7, type = ['fire']},
-  {name = 'Squirtle', height = 50, weight = 19.8, type = ['water']},
-  {name = 'Alakazam', height = 150, weight = 105.8, type = ['psychic']},
-  {name = 'Gengar', height = 150, weight = 89.3, type = ['ghost','posion']},
-  {name = 'Hitmonchan', height = 140, weight = 110.7, type = ['fighting']},
-  {name = 'Gyarados', height = 650, weight = 518.1, type = ['water','flying']},
-  {name = 'Snorlax', height = 210, weight = 1014.1, type = ['normal']},
-  {name = 'Dragonite', height = 220, weight = 463, type = ['dragon', 'flying']}
+  {name: 'Bulbasur', height: 70, weight: 15.2, type: ['grass','poison']},
+  {name: 'Charmander', height: 60, weight: 18.7, type: ['fire']},
+  {name: 'Squirtle', height: 50, weight: 19.8, type: ['water']},
+  {name: 'Alakazam', height: 150, weight: 105.8, type: ['psychic']},
+  {name: 'Gengar', height: 150, weight: 89.3, type: ['ghost','posion']},
+  {name: 'Hitmonchan', height: 140, weight: 110.7, type: ['fighting']},
+  {name: 'Gyarados', height: 650, weight: 518.1, type: ['water','flying']},
+  {name: 'Snorlax', height: 210, weight: 1014.1, type: ['normal']},
+  {name: 'Dragonite', height: 220, weight: 463, type: ['dragon', 'flying']}
 ];

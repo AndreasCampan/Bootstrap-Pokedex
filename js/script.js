ff006fd554812cf3064b5da999e3656f29b299fe
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-02-18 17:54:00 -0600

    Updated the script file as per new task
    -Added a function to create buttons with the pokedex database information (name) within an unordered list.
    -Created an event listener for more details upon clicking the pokemon

--- js/script.js
+++ js/script.js
@@ -11,41 +11,61 @@ let pokemonRepo = (function(){
     {name: 'Snorlax', height: 210, weight: 1014.1, type: ['Normal']},
     {name: 'Dragonite', height: 220, weight: 463, type: ['Dragon', ' Flying']}
   ];
-  // Adds pokemon to the pokedex - contains a datatype check
+
+  // Funtion to add pokemon to the pokedex - contains a datatype check
   function add(pokemon) {
     if (typeof pokemon === 'object' && typeof pokemon !== null) {
         pokedex.push(pokemon);
       } else {
         console.log('you need an object');
       }
-    }
-    // Function to allow the retrival of the pokedex data
-    function getAll() {
-      return pokedex;
-    }
+  }
 
-    return {
-      addf: add,
-      getAllf: getAll
-    };
+  // Function for the retrival of the pokedex data
+  function getAll() {
+    return pokedex;
+  }
+
+  // Function that will log the pokemon name to console when called
+  function showDetails(pokemon) {
+    console.log(pokemon.name + " Test");
+  }
+
+  /* Function to display pokemon from database on webpage. Contains a forEach method which creates a button with the name of each element iterated over */
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-list-style');
+    //event listener for on click to run the showDetails function
+    button.addEventListener('click', showDetails(pokemon));
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+  };
+
+  return {
+    addf: add,
+    getAllf: getAll,
+    addListItemf: addListItem
+  };
   })();
 
 pokemonRepo.addf({name: 'Mew', height: 41, weight: 8.8, type: 'Psychic'});
-  
-//Using the .forEach method, this is iterating over the elements in the pokedex array and deciding based on weight parameters how to write them out
 
 pokemonRepo.getAllf().forEach(function(pokemon) {
-  if (pokemon.weight > 500 && pokemon.weight < 1000) {
+  pokemonRepo.addListItemf(pokemon);
+});
+
+/*
+ if (pokemon.weight > 500 && pokemon.weight < 1000) {
     document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Weight: ${pokemon.weight} lbs - Now that is heavy! <br/> <br/>`);
   } else if (pokemon.weight > 1000) {
     document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Weight: ${pokemon.weight} lbs - Whoa IT'S HUGE!!! <br/> <br/>`);
   } else {
     document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Weight: ${pokemon.weight} lbs <br/> <br/>`); 
   }
-});
 
-
-/*
 The old for loop to iterate over every element in the array and then write it on the website. There's also an if logic that will look at the weight of the pokemon and add extra text depending on the parameters.
 
 for (let i=0; i < pokedex.length; i++){
